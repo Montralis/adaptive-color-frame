9deab6ee9b1b44ba5a920d6cb8a5e4e85a264faa
Leon Thomanek <Leon.Thomanek@dfs-as.aero>
2023-08-27 22:13:49 +0200

    feat(prod mode): add a demo and a prod mode

--- main.py
+++ main.py
@@ -1,93 +1,153 @@
 import os
+import sys
 import requests
 import uuid
 import time
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
+from dotenv import load_dotenv
+import argparse
 
-# Create the "img" folder if it doesn't exist
-if not os.path.exists('img'):
-    os.makedirs('img')
+# Load environment variables from .env file
+load_dotenv()
 
-# Delete all existing images in the "img" folder
-existing_images = os.listdir('img')
-for image_file in existing_images:
-    image_path = os.path.join('img', image_file)
-    os.remove(image_path)
-    print(f"Deleted existing image: {image_path}")
+# Read environment variables
+BORDER_TOP = int(os.getenv("BORDER_TOP", 100))
+BORDER_LEFT = int(os.getenv("BORDER_LEFT", 100))
+BORDER_RIGHT = int(os.getenv("BORDER_RIGHT", 100))
+BORDER_BOTTOM = int(os.getenv("BORDER_BOTTOM", 190))
+BORDER_COLOR_R = int(os.getenv("BORDER_COLOR_R", 255))
+BORDER_COLOR_G = int(os.getenv("BORDER_COLOR_G", 255))
+BORDER_COLOR_B = int(os.getenv("BORDER_COLOR_B", 255))
+TEXT = os.getenv("TEXT", "ISO 600; 1/12s, 2.1f")
+TEXT_COLOR_R = int(os.getenv("TEXT_COLOR_R", 0))
+TEXT_COLOR_G = int(os.getenv("TEXT_COLOR_G", 0))
+TEXT_COLOR_B = int(os.getenv("TEXT_COLOR_B", 0))
+FONT_SIZE = int(os.getenv("FONT_SIZE", 20))
 
-# Number of images to be downloaded
-num_images = 5
 
-for i in range(num_images):
-    try:
-        # Generate a random UUID for the ID
-        image_id = str(uuid.uuid4())
+def process_image(img):
+    # Calculate the new height for the portrait aspect ratio (3:4)
+    new_height = int(img.width * 4 / 3)
 
-        # Generate the timestamp in the format MMSS
-        timestamp = datetime.now().strftime("%M%S")
+    # Crop the image to the new aspect ratio
+    img = img.crop((0, 0, img.width / 2.5, new_height / 2.5))
 
-        # Lorem Picsum API URL for a random image
-        image_url = f"https://picsum.photos/1920/1080?random={i}"
+    # Save the cropped image with a border
+    img_with_border = Image.new(
+        'RGB',
+        (img.width + BORDER_LEFT + BORDER_RIGHT,
+         img.height + BORDER_TOP + BORDER_BOTTOM),
+        (BORDER_COLOR_R, BORDER_COLOR_G, BORDER_COLOR_B)
+    )
+    img_with_border.paste(img, (BORDER_LEFT, BORDER_TOP))
 
-        # Adjust the filename accordingly (ID_timestamp.png)
-        image_filename = f"img/{image_id}_{timestamp}.png"
+    # Add the text
+    draw = ImageDraw.Draw(img_with_border)
+    font = ImageFont.truetype("RobotoSlab-Medium.ttf", FONT_SIZE)
+    text_width = draw.textlength(TEXT, font=font)
+    text_x = (img_with_border.width - text_width) // 2
+    draw.text((text_x, img_with_border.height -
+               BORDER_BOTTOM + 15), TEXT, font=font, fill=(TEXT_COLOR_R, TEXT_COLOR_G, TEXT_COLOR_B))
 
-        # Download the image from the API URL
-        response = requests.get(image_url)
-        if response.status_code == 200:
-            with open(image_filename, 'wb') as f:
-                f.write(response.content)
-            print(f"Downloaded image {i+1}/{num_images}: {image_filename}")
+    return img_with_border
 
-            # Open the image using Pillow
-            img = Image.open(image_filename)
 
-            # Calculate the new height for the portrait aspect ratio (3:4)
-            new_height = int(img.width * 4 / 3)
+def download_image(image_url, image_filename):
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        with open(image_filename, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded image: {image_filename}")
+        return True
+    else:
+        print(f"Error downloading image")
+        return False
 
-            # Crop the image to the new aspect ratio
-            img = img.crop((0, 0, img.width / 2.5, new_height / 2.5))
 
-            # Save the cropped image
-            # Add a 1 cm white border
-            top_border = left_border = right_border = 100  # 1 cm in pixels
-            bottom_border = 190  # Slightly larger bottom border
-            border_color = (255, 255, 255)  # White color
-            img_with_border = Image.new(
-                'RGB',
-                (img.width + left_border + right_border,
-                 img.height + top_border + bottom_border),
-                border_color
-            )
-            img_with_border.paste(img, (left_border, top_border))
+def main():
+    parser = argparse.ArgumentParser(
+        description="Process images in demo or production mode.")
+    parser.add_argument("--demo", action="store_true", help="Run in demo mode")
+    parser.add_argument("--prod", action="store_true",
+                        help="Run in production mode")
+    args = parser.parse_args()
 
-            # Add the text
-            draw = ImageDraw.Draw(img_with_border)
-            text = "ISO 600; 1/12s, 2.1f"
-            text_color = (0, 0, 0)  # Black color for the text
+    if not args.demo and not args.prod:
+        print("Please specify either --demo or --prod flag.")
+        sys.exit(1)
 
-            # Load a font and define font size
-            font_size = 20
-            font = ImageFont.truetype("RobotoSlab-Medium.ttf", font_size)
+    if args.demo and args.prod:
+        print("Please specify only one mode using either --demo or --prod flag.")
+        sys.exit(1)
 
-            # Calculate the position of the text at the bottom border
-            text_width = draw.textlength(text, font=font)
-            text_x = (img_with_border.width - text_width) // 2
+    if args.demo:
+        print("Running in demo mode...")
+        image_source_folder = "demoImg"
+    else:
+        print("Running in production mode...")
+        image_source_folder = "img"
 
-            # Draw the text on the image
-            draw.text((text_x, img_with_border.height -
-                       bottom_border + 15), text, font=font, fill=text_color)
+    if args.prod:
 
-            # Save the image with the border
-            img_with_border.save(image_filename)
-            print(f"Cropped image saved: {image_filename}")
+        if not os.path.exists('img') or not  os.listdir('img'):
+            print(f"Please provide images im folder 'img' ")
 
-        else:
-            print(f"Error downloading image {i+1}/{num_images}")
+        existing_images = os.listdir('img')
+        for image_file in existing_images:
+            try:
+                timestamp = datetime.now().strftime("%M%S")
+                image_filename = f"img/{image_file[:len(image_file) - 4]}_{timestamp}.png"
 
-        # Pause for 1 second between downloads to avoid server load
-        time.sleep(1)
+                # In production mode, copy images from the image_source_folder
+                source_image_path = os.path.join(
+                    image_source_folder, image_file)
+                img = Image.open(source_image_path)
+                img_with_border = process_image(img)
+                img_with_border.save(image_filename)
+                print(
+                    f"Processed image: {source_image_path}")
 
-    except Exception as e:
-        print(f"Error occurred: {e}")
+                # Pause for 1 second between processing to simulate production load
+                time.sleep(1)
+
+            except Exception as e:
+                print(f"Error occurred: {e}")
+    else:
+        num_images = 5
+        # Create the "img" folder if it doesn't exist
+        if not os.path.exists('demoImg'):
+            os.makedirs('demoImg')
+
+        # Delete all existing images in the "img" folder
+        existing_images = os.listdir('demoImg')
+        for image_file in existing_images:
+            image_path = os.path.join('demoImg', image_file)
+            os.remove(image_path)
+            print(f"Deleted existing image: {image_path}")
+
+        for i in range(num_images):
+            try:
+
+                image_id = str(uuid.uuid4())
+                timestamp = datetime.now().strftime("%M%S")
+                image_url = f"https://picsum.photos/1920/1080?random={i}"
+                image_filename = f"demoImg/{image_id}_{timestamp}.png"
+
+                # In demo mode, download and process images from the internet
+                if download_image(image_url, image_filename):
+                    img = Image.open(image_filename)
+                    img_with_border = process_image(img)
+                    img_with_border.save(image_filename)
+                    print(
+                        f"Processed image {i + 1}/{num_images}: {image_filename}")
+
+                # Pause for 1 second between processing to avoid server load
+                time.sleep(1)
+
+            except Exception as e:
+                print(f"Error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
